95d08531a1071e112dd3bc09d757f7ad78ebd6a7
Research Analyzer <test@example.com>
2026-03-26 11:42:42 +0530

    Update assets and project imports

--- src/assets/index.js
+++ src/assets/index.js
@@ -47,7 +47,11 @@ import quizme from './quizme.png';
 import cneh from './cneh.png';
 import mipp from './mipp.png';
 import iots from './iots.png';
-import researchAnalyzer from "./autonomous_research_paper_analyzer.png";
+import marketEasy from './MarketEasy.png';
+import ethos from './tech/ethos.png';
+import vnrvjiet from './company/vnrvjiet.jpeg';
+import gioe from './company/gioe.jpg';
+import researchAnalyzer from './autonomous_research_paper_analyzer.png';
 
 
 export {
